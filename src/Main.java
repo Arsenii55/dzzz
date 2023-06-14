4f4f546282a72cd20273c52405d92144c78c0aa2
Arsenii55 <arseka2512@gmail.com>
2023-06-14 18:58:06 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int b = 2;
+        switch (b){
+            case 1:
+                System.out.println("Не верно");
+                break;
+            case 2:
+                System.out.println("В точку");
+                break;
+            case 3:
+                System.out.println("Не верно");
+                break;
+            default:
+                System.out.println("Нет верного ответа");
+        }
     }
 }
